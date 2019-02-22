84204ea3eefeb67ea4f0aedf08186d65aceaa058
Your Name <you@example.com>
2019-02-22 12:05:37 +0100

    new

--- k18:kserver/install.sh
+++ k18:kserver/install.sh
@@ -7,11 +7,16 @@ cp /opt/docker/kadm5.acl /var/kerberos/krb5kdc/kadm5.acl
 
 /usr/sbin/kdb5_util create -s -P masterkey
 
-kadmin.local addprinc -pw kisx47983457	isx47983457
-kadmin.local addprinc -pw kpere pere
-kadmin.local addprinc -pw kpau pau
-kadmin.local addprinc -pw kanna anna
-kadmin.local addprinc -pw kmarta marta/admin
-kadmin.local addprinc -pw kjordi jordi
-kadmin.local addprinc -pw superuser superuser
-kadmin.local addprinc -pw kjulia julia 
+kadmin.local -q "addprinc -pw kisx47983457 isx47983457"
+kadmin.local -q "addprinc -pw kpere pere"
+kadmin.local -q "addprinc -pw kpau pau"
+kadmin.local -q "addprinc -pw kanna anna"
+kadmin.local -q "addprinc -pw kmarta marta/admin"
+kadmin.local -q "addprinc -pw kjordi jordi"
+kadmin.local -q "addprinc -pw superuser superuser"
+kadmin.local -q "addprinc -pw kjulia julia"
+kadmin.local -q "addprinc -pw kuser01 user01"
+kadmin.local -q "addprinc -pw kuser02 user02"
+kadmin.local -q "addprinc -pw kuser03 user03"
+kadmin.local -q "addprinc -randkey sshd.edt.org" 
+
--- k18:sshd/install.sh
+++ k18:sshd/install.sh
@@ -1,16 +1,18 @@
 #! /bin/bash
 
 cp /opt/docker/krb5.conf /etc/krb5.conf
+cp /opt/docker/sshd_config /etc/ssh/sshd_config
+#cp /opt/docker/krb5.keytab /etc/krb5.keytab
 
 groupadd local01
-groupadd local02
+groupadd kusers
 
 useradd -g users -G local01 local01
 useradd -g users -G local01 local02
 useradd -g users -G local01 local03
-useradd -g users -G local02 local04
-useradd -g users -G local02 local05
-useradd -g users -G local02 local06
+useradd -g users -G kusers user01
+useradd -g users -G kusers user02
+useradd -g users -G kusers user03
 
 
 echo "local01" | passwd --stdin local01
@@ -18,5 +20,5 @@ echo "local02" | passwd --stdin local02
 echo "local03" | passwd --stdin local03
 
 ssh-keygen -A
-
+kadmin -p marta/admin -w kmarta -q "ktadd -k /etc/krb5.keytab host/sshd.edt.org"
 
